42e6c611e5fda0b5960f58980f9c3340e8466779
Yusuf Supriadi <yusufdolenk2@gmail.com>
2024-11-18 03:52:36 +0700

    post and auth

---
+++ migration/src/lib.rs
@@ -0,0 +1,20 @@
+#![allow(elided_lifetimes_in_paths)]
+#![allow(clippy::wildcard_imports)]
+pub use sea_orm_migration::prelude::*;
+
+mod m20220101_000001_users;
+
+mod m20241115_174833_posts;
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![
+            // inject-below (do not remove this comment)
+            Box::new(m20241115_174833_posts::Migration),
+            Box::new(m20220101_000001_users::Migration),
+            // inject-above (do not remove this comment)
+        ]
+    }
+}
